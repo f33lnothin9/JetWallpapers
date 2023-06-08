8a3bed0e792f8287fd3a508495dfb1e90e5caf9c
Nikita Bulavin <nik.bulavin.005@gmail.com>
2023-06-08 09:49:16 +0300

    Update Theme

---
+++ app/src/main/java/ru/nikitabulavin/jetwallpapers/core/JetWallpapersApp.kt
@@ -0,0 +1,7 @@
+package ru.nikitabulavin.jetwallpapers.core
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class JetWallpapersApp : Application()
--- app/src/main/java/ru/nikitabulavin/jetwallpapers/core/presentation/MainActivity.kt
+++ app/src/main/java/ru/nikitabulavin/jetwallpapers/core/presentation/MainActivity.kt
@@ -1,9 +1,9 @@
-package ru.nikitabulavin.jetwallpapers
+package ru.nikitabulavin.jetwallpapers.core.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import ru.nikitabulavin.jetwallpapers.ui.theme.JetWallpapersTheme
+import ru.nikitabulavin.jetwallpapers.core.presentation.ui.theme.JetWallpapersTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
